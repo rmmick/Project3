9f1a5cff0ea03cf19af8bc14515cfdb633ec2bc2
Rachel McCown <rachel.mccown.14@cnu.edu>
2017-03-22 18:20:01 -0400

    Working on JSON Downloads

--- app/src/main/java/com/example/rachel/project3/MainActivity.java
+++ app/src/main/java/com/example/rachel/project3/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
         myCheck = new ConnectivityCheck(this);
         doNetworkCheck();
 
